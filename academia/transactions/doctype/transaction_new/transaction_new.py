f7e9564bb45f77a350a53c901afbfb0afcb7fb49
hamedshehab <Hamedshehab2001@gmail.com>
2025-01-25 19:41:47 +0300

    feat(transactions): added trasfer transaction and create sub transaction buttons

--- academia/transactions/doctype/transaction_new/transaction_new.py
+++ academia/transactions/doctype/transaction_new/transaction_new.py
@@ -12,14 +12,19 @@ class TransactionNew(Document):
 	from typing import TYPE_CHECKING
 
 	if TYPE_CHECKING:
+		from academia.transactions.doctype.sub_transactions.sub_transactions import SubTransactions
 		from academia.transactions.doctype.transaction_related_documents.transaction_related_documents import TransactionRelatedDocuments
 		from frappe.types import DF
 
 		amended_from: DF.Link | None
+		company: DF.Link | None
+		is_sub_transaction: DF.Check
 		naming_series: DF.Literal["TRA-.YY.-.MM.-"]
+		parent_transaction: DF.Link | None
 		related_documents: DF.Table[TransactionRelatedDocuments]
 		start_date: DF.Date | None
 		status: DF.Literal["Pending", "Completed", "Canceled", "Closed", "Rejected"]
+		sub_transactions: DF.Table[SubTransactions]
 		title: DF.Data
 		transaction_holder: DF.Link | None
 	# end: auto-generated types
